9a9c6602af648189e37aa4f66639687fd66ee6c1
Ujjwal Singla <usingla@mun.ca>
2022-11-17 14:46:25 -0330

    last commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import './App.css';
-import Nav from "./nav";
+import "./App.css"
+import Nav from "./nav"
 
-function App() {
-  let user=[
-    {name:'anil',id:1},
-    {name:'peter',id:2},
-    {name:'bruce',id:3},
-    {name:'tony',id:4},
-  ]
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./home.js"
+import About from "./about.js"
+import Error from "./404.js"
+import User from "./user.js"
+let data
+let App = () => {
+	// let res = await fetch("https://api.agify.io?name=bella")
+	// let data = await res.json()
+	// console.log(data.age)
 
-  return (
-    <div className="App">
-      <h1>Ujjwal</h1>
-      <Nav />
+	let user = [
+		{ name: "anil", id: 1 },
+		{ name: "peter", id: 2 },
+		{ name: "bruce", id: 3 },
+		{ name: "tony", id: 4 },
+	]
 
-    </div>
-  );
+	return (
+		<div className="App">
+			{user.map((item, i) => (
+				<div key={i}>
+					<Link key={i} to={"/user/" + item.id}>
+						{item.id}
+					</Link>
+				</div>
+			))}
+			<Nav />
+			<Routes>
+				<Route path="/" exact="true" element={<Home />}></Route>
+				<Route path="/about" exact="true" element={<About />}></Route>
+				<Route path="*" element={<Error />}></Route>
+				<Route path="/user/:id" render={() => <User />} element={<User />}></Route>
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/user.js
+++ src/user.js
@@ -1,16 +1,33 @@
-//import React, { useState } from "react"
 import { useParams } from "react-router-dom"
-function User() {
-	let { slug } = useParams()
-	console.log(slug)
+import { useEffect, useState } from "react"
+function User(props) {
+	let [data, setData] = useState([])
+	let { id } = useParams()
+	useEffect(() => {
+		fetch("https://api.agify.io?name=bella").then((res) => res.json().then((res) => setData(res)))
+	}, [])
+	console.log(data)
 	return (
 		<div>
-			<h1>Users</h1>
-			{/* <button onClick={() => props.data()}>Message for tyson</button> */}
+			<h1>User No {data.name}</h1>
 		</div>
 	)
 }
+
 export default User
+//import React, { useState } from "react"
+// import { useParams } from "react-router-dom"
+// function User() {
+// 	let { slug } = useParams()
+// 	console.log(slug)
+// 	return (
+// 		<div>
+// 			<h1>Users</h1>
+// 			{/* <button onClick={() => props.data()}>Message for tyson</button> */}
+// 		</div>
+// 	)
+// }
+// export default User
 // import React, { Component } from "react";
 // class user extends Component {
 // 	constructor() {
